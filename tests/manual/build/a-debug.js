760beb21410b7fefedfdea92b275fe386fd3d2d9
yiminghe <yiminghe@gmail.com>
2014-08-20 19:12:41 +0800

    simply works

---
+++ tests/manual/build/a-debug.js
@@ -0,0 +1,16 @@
+/*
+combined modules:
+lib/a
+lib/b
+*/
+modulex.add('lib/a', [
+    './b',
+    'c'
+], function (require, exports, module) {
+    var b = require('./b');
+    var c = require('c');
+    module.exports = b + 1 + c;
+});
+modulex.add('lib/b', [], function (require, exports, module) {
+    module.exports = 1;
+});
---
+++ tests/manual/lib/a.js
@@ -0,0 +1,3 @@
+var b = require('./b');
+var c = require('c');
+module.exports = b + 1 + c;
