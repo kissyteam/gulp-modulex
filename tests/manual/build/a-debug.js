61ae6a60537c03e8cf6de2f7eade1a86c1256e7c
yiminghe <yiminghe@gmail.com>
2014-08-21 23:13:56 +0800

    remove esprima

--- tests/manual/build/a-debug.js
+++ tests/manual/build/a-debug.js
@@ -3,14 +3,7 @@ combined modules:
 lib/a
 lib/b
 */
-modulex.add('lib/a', [
-    './b',
-    'c'
-], function (require, exports, module) {
-    var b = require('./b');
-    var c = require('c');
-    module.exports = b + 1 + c;
-});
-modulex.add('lib/b', [], function (require, exports, module) {
-    module.exports = 1;
-});
+modulex.add("lib/a", ["lib/b","c"], function(require,exports,module){var b = require('./b');
+var c = require('c');
+module.exports = b + 1 + c;});
+modulex.add("lib/b", [], function(require,exports,module){module.exports = 1;});
--- tests/manual/gulpfile.js
+++ tests/manual/gulpfile.js
@@ -5,8 +5,10 @@ var path = require('path');
 gulp.task('default', function () {
     gulp.src('./lib/a.js')
         .pipe(gulpModulex({
-            packages: {
-                lib: {base: path.resolve(process.cwd(), './lib')}
+            modulex: {
+                packages: {
+                    lib: {base: path.resolve(process.cwd(), './lib')}
+                }
             }
         }))
         .pipe(gulp.dest('./build'));
